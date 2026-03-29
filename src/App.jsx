9384af43d2445af2b3cded26aae8c2744e6b5f0b
Herik25 <harshparmar87990@gmail.com>
2026-03-29 23:53:14 +0530

    feat: add image assets and enhance App component with new sections and titles

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,17 @@ function App() {
   return (
     <>
       <main>
+        <div className="images">
+          <img id="image-1" src="/image-1.png" alt="image 1" />
+          <img id="image-2" src="/image-2.png" alt="image 2" />
+          <img id="image-3" src="/image-3.png" alt="image 3" />
+          <img id="image-4" src="/image-4.png" alt="image 4" />
+          <img id="image-5" src="/image-5.png" alt="image 5" />
+          <img id="image-6" src="/image-6.png" alt="image 6" />
+          <img id="image-7" src="/image-7.png" alt="image 7" />
+        </div>
         <Canvas
+          id="canvas-element"
           style={{
             height: "100vh",
             width: "100vw",
@@ -15,8 +25,6 @@ function App() {
             top: 0,
             left: 0,
             zIndex: 1,
-            backgroundImage: "url('/background-1.png')",
-            backgroundSize: "cover",
           }}
         >
           <Dog />
@@ -49,7 +57,38 @@ function App() {
             </div>
           </div>
         </section>
-        <section id="sectoin-2"></section>
+        <section id="section-2">
+          <div className="titles">
+            <div img-title="image-1" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>COOL SHADDER 1</h1>
+            </div>
+            <div img-title="image-2" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>COOL SHADDER 1</h1>
+            </div>
+            <div img-title="image-3" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>COOL SHADDER 1</h1>
+            </div>
+            <div img-title="image-4" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>COOL SHADDER 3</h1>
+            </div>
+            <div img-title="image-5" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>COOL SHADDER 4</h1>
+            </div>
+            <div img-title="image-6" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>COOL SHADDER 5</h1>
+            </div>
+            <div img-title="image-7" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>COOL SHADDER 6</h1>
+            </div>
+          </div>
+        </section>
         <section id="sectoin-3"></section>
       </main>
     </>
